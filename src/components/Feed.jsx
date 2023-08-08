19c135e49d76e599782f804059946d14622397c9
dhruvin <dhruvin.200410116086@gmail.com>
2023-08-09 01:10:45 +0530

    sideNav

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,13 @@
+import React from 'react'
+import { useEffect, useContext } from "react"
+import { Context } from "../context/contextApi"
 
+import Leftnav from './Leftnav'
 const Feed = () => {
 
     return (
-        <div>
-            your feed here
+        <div className='flex flex-row h-[clac(100%-56px)]'>
+            <Leftnav/>
         </div>
     )
 }
